82e60907d1da2c1718d43cb970b3baf9f4a73521
samhud123 <git@stackblitz.com>
2025-02-15 09:02:55 +0700

    Initial commit

---
+++ app/contacts/page.tsx
@@ -0,0 +1,12 @@
+import ContactTable from '@/components/contact-table';
+
+const Contacts = () => {
+  return (
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5"></div>
+      <ContactTable />
+    </div>
+  );
+};
+
+export default Contacts;
---
+++ components/search.tsx
@@ -0,0 +1,13 @@
+const Search = () => {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        className="w-full border border-gray-200 py-2 pl-10 text-sm outline-2 rounded-sm"
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
+
+export default Search;
